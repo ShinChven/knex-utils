214ede204c78ec365853dfc4e3e65b0d0b4e8e3e
ShinChven <shinchven@gmail.com>
2021-09-17 13:56:05 +0800

    update upsert

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,12 @@ import {Knex} from 'knex';
  * @param key select key
  */
 export const insertIfNotExist = async <T>(knex: Knex, tablename: string, data: Partial<T>, where: Partial<T>, key?: string): Promise<number> => {
-    const found = await knex(tablename).select(key || '*').where(where);
-    if (found.length === 0) {
-        const [result] = await knex(tablename).insert(data);
-        return result;
-    }
-    return 0;
+  const found = await knex(tablename).select(key || '*').where(where);
+  if (found.length === 0) {
+    const [result] = await knex(tablename).insert(data);
+    return result;
+  }
+  return 0;
 };
 
 /**
@@ -26,13 +26,14 @@ export const insertIfNotExist = async <T>(knex: Knex, tablename: string, data: P
  * @param key select key
  */
 export const upsert = async <T>(knex: Knex, tablename: string, data: Partial<T>, where: Partial<T>, key?: string): Promise<number> => {
-    const found = await knex(tablename).select(key || '*').where(where);
-    if (found.length === 0) {
-        await knex(tablename).insert(data);
-    } else {
-        await knex(tablename).update(data).where(where);
-    }
-    return found[0]?.id || 0;
+  const found = await knex(tablename).select(key || '*').where(where).limit(1);
+  if (found.length === 0) {
+    const [created] = await knex(tablename).insert(data);
+    return created;
+  } else {
+    await knex(tablename).update(data).where(where);
+  }
+  return found[0]?.id || 0;
 };
 
 /**
@@ -42,8 +43,8 @@ export const upsert = async <T>(knex: Knex, tablename: string, data: Partial<T>,
  * @param id id
  */
 export const getById = async <T>(knex: Knex, tablename: string, id: number | string): Promise<T> => {
-    const [result] = await knex(tablename).where('id', id).limit(1);
-    return result as T;
+  const [result] = await knex(tablename).where('id', id).limit(1);
+  return result as T;
 };
 
 /**
@@ -53,6 +54,6 @@ export const getById = async <T>(knex: Knex, tablename: string, id: number | str
  * @param data data
  */
 export const insertAndReturn = async <T>(knex: Knex, tablename: string, data: Partial<T>): Promise<T> => {
-    const [id] = await knex(tablename).insert(data);
-    return getById<T>(knex, tablename, id);
+  const [id] = await knex(tablename).insert(data);
+  return getById<T>(knex, tablename, id);
 };
