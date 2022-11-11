fa9f7eb73d43f58902fbf1a87bee202cf08dcf67
ShinChven <shinchven@gmail.com>
2022-11-11 14:04:38 +0800

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import {Knex} from 'knex';
-
 /**
  * insert a record if not exist
  * @param knex Knex
@@ -8,8 +6,10 @@ import {Knex} from 'knex';
  * @param where fields to match record
  * @param key select key
  */
+import {Knex} from 'knex';
+
 export const insertIfNotExist = async <T>(knex: Knex, tablename: string, data: Partial<T>, where: Partial<T>, key?: string): Promise<number> => {
-  const found = await knex(tablename).select(key || '*').where(where);
+  const found = await knex<T>(tablename).select(key || '*').where(where);
   if (found.length === 0) {
     const [result] = await knex(tablename).insert(data);
     return result;
@@ -17,34 +17,44 @@ export const insertIfNotExist = async <T>(knex: Knex, tablename: string, data: P
   return 0;
 };
 
-/**
- * update or insert a record
- * @param knex Knex
- * @param tablename tableName
- * @param data data to insert
- * @param where fields to match record
- * @param key select key
- */
-export const upsert = async <T>(knex: Knex, tablename: string, data: Partial<T>, where: Partial<T>, key?: string): Promise<number> => {
-  const found = await knex(tablename).select(key || '*').where(where).limit(1);
-  if (found.length === 0) {
-    const [created] = await knex(tablename).insert(data);
-    return created;
+export type UpsertAndReturnProps<T> = {
+  tableName: string;
+  data: Partial<T>;
+  where: Partial<T>;
+  returnRecord?: boolean;
+  knex: Knex;
+}
+
+export const upsert
+  = async <T>({tableName, where, data, returnRecord, knex}: UpsertAndReturnProps<T>): Promise<T | void> => {
+
+  const existed = await knex<T>(tableName).select('id').where(where).first() as { id: number };
+  let id: number;
+  if (existed && existed.id) {
+    console.log('existed');
+    await knex<T>(tableName).update(data).where({id: existed.id});
+    id = existed.id;
   } else {
-    await knex(tablename).update(data).where(where);
+    console.log('not existed');
+    const [created] = await knex<T>(tableName).insert(data);
+    id = created;
   }
-  return found[0]?.id || 0;
+
+  if (returnRecord === true) {
+    return knex(tableName).select('*').where({id}).first() as T;
+  }
+
+  return Promise.resolve();
 };
 
 /**
- * get a record by id
+ * Get a record by id
  * @param knex Knex
  * @param tablename tableName
  * @param id id
  */
 export const getById = async <T>(knex: Knex, tablename: string, id: number | string): Promise<T> => {
-  const [result] = await knex(tablename).where('id', id).limit(1);
-  return result as T;
+  return await knex(tablename).where('id', id).first() as T;
 };
 
 /**
